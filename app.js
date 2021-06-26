8fb2a43bcfb3db741962ad489a161050924706c1
Vadzim <vadzimk@hotmail.com>
2021-06-26 10:30:31 -0700

    11.11 Health check

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change to make sure new version is deployed
+})
+
 app.listen(PORT, () => {
-  // console.log('server started on port 5000')
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
 })
