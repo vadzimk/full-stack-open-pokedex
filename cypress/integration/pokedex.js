8c343ad7f2a7786ab2f5a9b6bf369fc37c687b93
Vadzim <vadzimk@hotmail.com>
2021-06-25 20:42:53 -0700

    11.9 Simple end to end -tests

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,7 +1,14 @@
 describe('Pokedex', function () {
+
   it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate from front page to ivysaur page', function (){
+    cy.visit('http://localhost:5000/pokemon/bulbasaur')
+    cy.contains('bulbasaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
